5ee7a6cb78ecc13b19fef23df1190ca93f3ae01e
zenojr <zfrancajunior@gmail.com>
2019-02-27 10:24:45 -0300

    correct

--- src/app/descontos/pipes/limpa-sufixo-cv.pipe.ts
+++ src/app/descontos/pipes/limpa-sufixo-cv.pipe.ts
@@ -7,7 +7,7 @@ export class LimpaSufixoCvPipe implements PipeTransform {
 
   transform(value: string, args?: any): string {
     let sufixoNovo: string = value;
-    // sufixoNovo = sufixoNovo.replace(/TAB|X72|P3/gi, "");
+    sufixoNovo = sufixoNovo.replace(/TAB|X72|P3/gi, "");
     
     // sufixoNovo =  sufixoNovo.substring(4,7);
     sufixoNovo = sufixoNovo.slice( 0, 7 );
