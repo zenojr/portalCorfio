bcdfa38433484c0a4515301ff99877e323052da5
zenojr <zfrancajunior@gmail.com>
2019-06-10 15:10:51 -0300

    morgan added

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+let morgan =  require('morgan');
 
 const app = express();
 const port = process.env.PORT || 4201;
+let count;
 
-app.use(express.static(__dirname + '/dist/portal' ));
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+// app.use(morgan('combined'));
+// app.use(morgan('tiny'));
+app.use(morgan(':method :url :response-time'));
+
+app.use(express.static(__dirname + '/dist/portal'));
+
+
+app.get('/*', (req, res) => 
+    {
+        res.sendFile(path.join(__dirname), count++),
+        count = req.query.PORT
+    });
 const server = http.createServer(app);
-server.listen(port, () => console.log('Portal Running...' ));
+server.listen(port, () => {
+    console.log('Portal Running...')
+     
+});
+
+
