c2d870ffc4327dfb7704a279eb0e1823348ac842
zenojr <zfrancajunior@gmail.com>
2019-06-13 11:52:11 -0300

    change filter

--- src/app/descontos/pipes/limpa-sufixo-select.pipe.ts
+++ src/app/descontos/pipes/limpa-sufixo-select.pipe.ts
@@ -8,7 +8,7 @@ export class LimpaSufixoSelectPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     let sufixoNovo: string = value;
-
+    console.log(value);
     sufixoNovo =  sufixoNovo.substring(0, 8);
     console.log('novoSuf' + sufixoNovo);
     return sufixoNovo;
