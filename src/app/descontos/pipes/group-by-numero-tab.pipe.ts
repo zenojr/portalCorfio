09161019cb87177f8ac88b97f01022febc18df21
zenojr <zfrancajunior@gmail.com>
2019-06-13 15:48:57 -0300

    change filter

--- src/app/descontos/pipes/group-by-numero-tab.pipe.ts
+++ src/app/descontos/pipes/group-by-numero-tab.pipe.ts
@@ -8,6 +8,13 @@ export class GroupByNumeroTabPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
+    // let sufixoNovo: string = value;
+    // console.log(value);
+    // // sufixoNovo =  sufixoNovo.substring(0, 6);
+    // sufixoNovo =  sufixoNovo.slice(0, 7);
+    // console.log('novoSuf' + sufixoNovo);
+    // return sufixoNovo;
+
     if (value !== undefined && value !== null) {
       // let newValue = value.slice(0, 6);
       let newValue: string = value;
