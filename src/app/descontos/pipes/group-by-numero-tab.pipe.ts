709df48aea271417c3863c52745db4855ee76a0d
zenojr <zfrancajunior@gmail.com>
2019-06-13 16:51:38 -0300

    change filter

--- src/app/descontos/pipes/group-by-numero-tab.pipe.ts
+++ src/app/descontos/pipes/group-by-numero-tab.pipe.ts
@@ -14,7 +14,7 @@ export class GroupByNumeroTabPipe implements PipeTransform {
       // let newValue: string = value;
       // newValue = newValue.slice(0, 7);
       // console.log(newValue);
-      value = value.slice(0, 8);
+      value = value.slice(0, 9);
       return _.uniqBy(value, 'sufixoCv');
     }
   }
