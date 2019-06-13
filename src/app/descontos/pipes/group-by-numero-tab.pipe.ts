7953e2771249d9c575f7308d657a80da4d7d9955
zenojr <zfrancajunior@gmail.com>
2019-06-13 15:02:35 -0300

    change filter

--- src/app/descontos/pipes/group-by-numero-tab.pipe.ts
+++ src/app/descontos/pipes/group-by-numero-tab.pipe.ts
@@ -8,10 +8,10 @@ export class GroupByNumeroTabPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
 
-    if(value !== undefined && value !== null) {
+    if (value !== undefined && value !== null) {
       // let newValue = value.slice(0, 6);
       let newValue: string = value;
-      newValue = newValue.slice(4, 7);
+      newValue = newValue.slice(0, 7);
       console.log(newValue);
       return _.uniqBy(newValue, 'sufixoCv');
     }
