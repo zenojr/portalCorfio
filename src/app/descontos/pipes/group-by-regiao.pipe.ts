2aa775c6e389339cf30e070aed9855c1748eae83
zenojr <zfrancajunior@gmail.com>
2019-09-23 17:59:23 -0300

    up

--- src/app/descontos/pipes/group-by-regiao.pipe.ts
+++ src/app/descontos/pipes/group-by-regiao.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class GroupByRegiaoPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if(value!== undefined && value!== null){
+    if ( value !== undefined && value !== null) {
       return _.uniqBy(value, 'regiao');
     }
     return value;
